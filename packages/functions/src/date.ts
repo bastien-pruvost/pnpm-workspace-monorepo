26420e4f70211e8d4b1c2880f47010fbc963c88c
Bastien Pruvost <bastien.mory@gmail.com>
2023-02-17 23:43:30 +0100

    feat: add subDays export in date.ts in "functions" package

--- packages/functions/src/date.ts
+++ packages/functions/src/date.ts
@@ -3,3 +3,5 @@ import { subDays, addDays as addDaysOld } from 'date-fns';
 export function addDays(date: number | Date, amount: number) {
   return addDaysOld(date, amount);
 }
+
+export { subDays };
